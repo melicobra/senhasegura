d6f6ffb370e180bb5de987037b5f9a263869fe84
melicobra <160254468+melicobra@users.noreply.github.com>
2025-06-27 11:04:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelectorAll('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+function aumentaTamanho(){
+    if(tamanhoSenha < 20)[
+        tamanhoSenha++;
+    ]
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
