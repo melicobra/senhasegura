4dcc1b0f68c98fb7ee199edd7dfca58f44196dff
melicobra <160254468+melicobra@users.noreply.github.com>
2025-08-01 11:43:27 -0300

    ok

--- main.js
+++ main.js
@@ -13,17 +13,38 @@ const forcaSenha = document.querySelector('forca');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if(tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
-function aumentaTamanho(){
-    if(tamanhoSenha < 20)[
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
         tamanhoSenha++;
-    ]
+    }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
+}
+
+function geraSenha() {
+    let alfabeto = '';
+    if (checkbox[0].checked) alfabeto += letrasMaiusculas;
+    if (checkbox[1].checked) alfabeto += letrasMinusculas;
+    if (checkbox[2].checked) alfabeto += numeros;
+    if (checkbox[3].checked) alfabeto += simbolos;
+    if (alfabeto.length === 0) {
+        campoSenha.value = '';
+        classificaSenha(1);
+        return;
+    }
+        let senha = '';
+        for (let i = 0; i < tamanhoSenha; i++);{
+        const numeroAleatorio = Math.floor(Math.random() * alfabeto.length);
+        senha += alfabeto[numeroAleatorio];
+}
+     campoSenha.value = senha;
+     classificaSenha(alfabeto.length);
 }
